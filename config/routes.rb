82e607c7e791ddadd8b27bd747dadea7459bd0b1
Fawwaz <fawwaz.0509@gmail.com>
2019-01-15 15:59:58 +0800

    Edit login/signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
 
-  
   root 'home#index'
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
   get 'signup', to: 'users#new', as: 'signup'
+  get 'signout' to: 'home', as: 'signout'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   get "/auth/:provider/callback" => "sessions#create_from_omniauth"
-
-  resources :users
   get "/search" => "entries#search", as: "search"
   resources :entries 
 
